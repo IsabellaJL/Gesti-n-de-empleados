beb5411a4a847d5c4df1b0fd146b6a754bc9367e
★Isa★ <isabellajimenezlazaro@gmail.com>
2025-11-15 12:41:58 -0500

    Update Employee.cs

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,12 +1,9 @@
 namespace SistemaGestionEmpleados.Models;
 
-public class Employee
+public class Employee : Person
 {
-    public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string IdNumber { get; set; }
-    public byte Age  { get; set; }
     public string Role { get; set; }
-    public double Salary { get; set; }
+    public decimal Salary { get; set; }
+    public string IdNumber { get; set; }
+
 }
