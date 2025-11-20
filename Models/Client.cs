7fd02e756271b1259fd299613c58bd07c0e3e0ef
★Isa★ <isabellajimenezlazaro@gmail.com>
2025-11-20 17:57:46 -0500

    Refactor Client class and add ShowInfo method

--- Models/Client.cs
+++ Models/Client.cs
@@ -1,8 +1,16 @@
-namespace DefaultNamespace;
-
-public class Client : Person
+namespace SistemaGestionEmpleados.Models
 {
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-    
+    public class Client : Person
+    {
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Client: {GetFirstName()} {GetLastName()}");
+            Console.WriteLine($"Age: {GetAge()}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Phone: {PhoneNumber}");
+        }
+    }
 }
